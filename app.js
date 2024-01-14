8598eff85d7e663e73fcb0b14c41e3ef82352fc9
JuandresQm <juandresmachado@outlook.es>
2024-01-13 22:20:51 -0400

    task 3

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var axios = require ('axios');
 
 
 console.log(process.env.USER);
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 require ('dotenv').config();
 var express = require('express');
 var router = express.Router();
+var axios = require ('axios');
 const session = require('express-session');
+
 const productosModel = require ('../models/admin')
+const { Axios } = require('axios');
+
 
 router.use(session({
   secret: 'secretkeys',
   resave: false,
   saveUninitialized: false
 }));
+
 //Get principal page
 router.get('/clientes', function(req, res, next){
   productosModel
@@ -21,6 +26,172 @@ router.get('/clientes', function(req, res, next){
       return res.status(500).send('Error cargando archivos')
     })
 })
+
+
+//Inicio de sesion clientes
+router.get('/loginclientes', function(req, res, next){
+  if (req.session.auth){
+    res.redirect('/clientes');
+  } else {
+  res.render('loginclientes', {title: 'Login Clientes'})}
+});
+
+//logeo de inicio de sesion cliente
+router.post('/login2', function(req, res, next){
+  const {email, password} =req.body;
+  let concat, concat2;
+  productosModel
+  .iniciosesionclientes(email)
+  .then(datos=>{
+    concat = datos[0].password;
+    concat2 = datos[0].id
+    console.log(concat2);
+    if (password == concat){
+      req.session.auth = true;
+      req.session.username = concat2;
+      res.redirect('/clientes');
+    }else{
+      res.send('esto no funciona')
+    }
+  })
+  .catch(err=>{
+    console.error(err.message);
+    return res.status(500).send('Error en el inicio de sesion')
+  })
+});
+
+//Pagina registros clientes
+router.get('/register-pag', function(req, res, next){
+  res.render('registerclientes', {title: 'Registro Clientes'})
+});
+
+//Registro de clientes
+router.post('/register', function(req, res, next){
+  const {email, password1, password2, preg_seg, resp_seg} = req.body;
+  if (password1 != password2){
+    res.redirect('/passwordfail')
+  } else{
+  productosModel
+    .registroclientes(email, password1, preg_seg, resp_seg)
+    .then(idClienteRegistrado=>{
+      res.redirect('/clientes')
+    })
+    .catch(err=>{
+      console.error(err.message);
+      return res.status(500).send('Error en el registro')
+    })}
+});
+
+//Pagina error en contraseñas
+router.get('/passwordfail', function(req, res, next){
+  res.render('clavefail', {title: 'Contraseñas incorrectas'})
+})
+
+//Pagina recuperar contraseña
+router.get('/recuperar', function(req, res, next){
+  res.render('recuperar', {title: 'Recuperar Contraseña'})
+});
+
+
+//Responder pregunta de Seguridad
+router.post('/resclave', function(req, res, next){
+  const {pregunta, respuesta} = req.body;
+  productosModel
+    .recuperarclave(pregunta, respuesta)
+    .then(datos=>{
+      res.render('claverec', {datos: datos});
+    })
+    .catch(err=>{
+      console.error(err.message);
+      return res.status(500).send('Pregunta y respuesta no coincidente')
+    })
+});
+
+
+//Pagina detalles productos
+router.get('/detallesprd/:id', function(req, res, next){
+  const id = req.params.id
+  productosModel
+    .obtenerPorId(id)
+    .then(datos=>{
+      res.render('detallesprd', {datos: datos});
+    })
+    .catch(err=>{
+      console.error(err.message);
+      return res.status(500).send('No se encuentra el producto')
+    })
+});
+
+//Pagina formulario de compra 
+router.get('/pedidoprd/:id', function(req, res, next){
+  if(req.session.auth){
+    const id = req.params.id;
+    productosModel
+      .obtenerPorId(id)
+      .then(datos=>{
+        res.render('pedidoprd', {datos: datos});
+      })
+      .catch(err=>{
+        console.error(err.message);
+        return res.status(500).send('No se encuentra el producto')
+      })
+  } else{
+    res.redirect('/loginclientes');
+  }
+})
+
+//Pago productos API 
+router.post('/payments', async (req, res, next)=>{
+  var monto, moneda;
+  const {producto_id, descripcion, nombre, numero_tarjeta, cvv, mes_ven, year_ven, moneda_id, cantidad, referencia, precio} = req.body;
+  const ip_cliente = req.socket.remoteAddress;
+  const cliente_id = req.session.username;
+  if (moneda_id == 1) {
+    moneda= 'USD';
+    monto = cantidad * precio;
+  }else{
+    if (moneda_id == 2) {
+      moneda= 'EUR';
+      monto = (cantidad * precio) * 0.91;
+    } else {
+      if (moneda_id == 3) {
+        moneda= 'VES';
+        monto = (cantidad * precio) * 35.94; 
+      }
+    }
+  }
+  const payments ={
+    "amount": monto,
+    "card-number": numero_tarjeta,
+    "cvv": cvv,
+    "expiration-month": mes_ven,
+    "expiration-year": year_ven,
+    "full-name": nombre,
+    "currency": moneda,
+    "description": descripcion,
+    "reference": referencia
+  }
+  try{
+    const response = await axios.post ('https://fakepayment.onrender.com/payments', payments, {headers:{ Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJkYXRlIjoiMjAyNC0wMS0xM1QyMTowNjo0Ny45MzhaIiwiaWF0IjoxNzA1MTgwMDA3fQ.SOmYtK2rkWWTeQ2zkmV5nUAo6WwFfcdT3UaUj-mkruY'}});
+    const data = JSON.parse(JSON.stringify(response.data));
+      const transaccion_id = data.data.transaction_id;
+      const total_pagado = data.data.amount;
+      const fecha = data.data.date;
+      const referencia = data.data.reference;
+      const descripcion = data.data.description;
+      const message = data.message;
+      console.log(message);
+      productosModel
+      .facturas(cantidad, total_pagado, fecha, ip_cliente, transaccion_id, descripcion, referencia, moneda_id, cliente_id, producto_id)
+      .then(idFacturaRealizada =>{
+        res.render('pagosuccess', {title: 'Compra Exitosa'})
+      })
+  } catch (err) {
+    res.render('pagofails');
+  }
+})
+
+
 
 
 
@@ -496,9 +667,51 @@ router.get('/deleteimg/:id', function(req,res,next){
  
 });
 
-/*Non-existent route
+
+//Vista tabla de compras
+router.get('/tablacompras', function(req, res, next){
+  if (req.session.active){
+    productosModel
+    .obtenerfacturas()
+    .then(facturas=>{
+      res.render('tablacompras', {facturas: facturas})
+    })
+    .catch(err=>{
+      console.error(err.message);
+      return res.status(500).send('Error cargando las facturas')
+    })
+  } else{
+    res.redirect('/index')
+  }
+});
+
+//Vista tabla de clientes
+router.get('/tablaclientes', function(req, res, next){
+  if (req.session.active){
+    productosModel
+    .obtenerclientes()
+    .then(clientes=>{
+      res.render('tablaclientes', {clientes: clientes})
+    })
+    .catch(err=>{
+      console.error(err.message);
+      return res.status(500).send('Error cargando los clientes')
+    })
+  } else{
+    res.redirect('/index');
+  }
+});
+
+//Cerrar sesion
+router.get('/logout', function (req, res, next){
+  req.session.destroy();
+  res.redirect('/');
+})
+
 router.get('/*', function(req, res, next) {
   res.render('error', { title: 'Error 404'});
 });
-*/
+
+
+
 module.exports = router;
